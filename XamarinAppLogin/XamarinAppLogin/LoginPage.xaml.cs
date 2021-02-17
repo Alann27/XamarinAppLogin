3866fe8cf2929b5994810d0ed685470cd64c48d0
Alan Rojas <alan2rm7@gmail.com>
2021-02-17 13:09:02 -0400

    Add project files.

---
+++ XamarinAppLogin/XamarinAppLogin/LoginPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinAppLogin
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : TabbedPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void BtnAceptar_Clicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(entEmail.Text) && !string.IsNullOrEmpty(entPassword.Text))
+            {
+
+                await DisplayAlert("INTEC App", $"Bienvenido, {entEmail.Text}!", "Aceptar");
+
+                await Navigation.PushModalAsync(new HomePage());
+
+
+            }
+            else
+            {
+                await DisplayAlert("INTEC App", "Campo Email y/o contraseña no puede estar vacío", "Aceptar");
+            }
+        }
+
+        private void Registro_Tapped(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new RegisterPage());
+        }
+    }
+}
---
+++ XamarinAppLogin/XamarinAppLogin/RegisterPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinAppLogin
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RegisterPage : ContentPage
+    {
+        public RegisterPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void BtnRegister_Clicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(entEmail.Text) && !string.IsNullOrEmpty(entName.Text) &&
+                !string.IsNullOrEmpty(entPassword.Text) && !string.IsNullOrEmpty(entConfirmPassword.Text))
+            {
+                if (entPassword.Text == entConfirmPassword.Text)
+                {
+                    await DisplayAlert("Register", $"Bienvenido, {entName.Text}!","OK");
+
+                    await Navigation.PushModalAsync(new HomePage());
+                }
+                else
+                {
+                    await DisplayAlert("Error", "Las contraseñas no coinciden, favor introducirlas nuevamente","OK");
+
+                    entPassword.Text = "";
+                    entConfirmPassword.Text = "";
+                    entPassword.Focus();
+                }
+            }
+            else
+            {
+               await DisplayAlert("Error","Faltan campos por llenar, favor verifique e intente de nuevo", "OK");
+            }
+        }
+    }
+}
